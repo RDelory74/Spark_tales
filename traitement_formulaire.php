22ee0047f7ba78363a463af1c88ec1ce09e2620a
RDelory74 <roro_spike@hotmail.com>
2024-06-26 16:56:55 +0200

    up bdd

--- traitement_formulaire.php
+++ traitement_formulaire.php
@@ -7,14 +7,16 @@
  $name = valid_info($_POST["name"]);
  $Raison = valid_info($_POST["Raison"]);
  $message = valid_info($_POST["message"]);
- //$civilite2 = ($_POST["civility"]);
+
  // Je concatène les valeurs de mes variables dans une seule variable
 
 $data = "Civilité: $civilite\n";
 $data .= "Nom: $name\n";
 $data .= "Email: $mail\n";
 $data .= "Raison de contact: $Raison\n";
-$data .= "Message:\n$message\n\n";
+$data .= "Message:$message\n\n";
+
+// Je néttoi mes données avec ma fonction valid_info
 
  function valid_info($infos){
     $infos= trim($infos);
@@ -38,14 +40,7 @@ $data .= "Message:\n$message\n\n";
     }
     else {
         echo "Formulaire pas valide !! ";
-       
         // Le formulaire n'a pas été soumis
-        // ...
     }
 // Code commun aux deux situations
-// ...
-
-
-
-
 ?>
